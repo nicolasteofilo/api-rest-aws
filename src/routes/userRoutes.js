685426ddbb942fe05263325f3204afdd9ed0f669
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-08-20 15:28:46 -0300

    update

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -19,6 +19,6 @@ export default router;
 index -> lista todos os usuários -> GET
 store/create -> cria um novo usuário -> POST
 delete -> apaga um usuário -> DELETE
-show -> mostra um usuário -> GET
-update -> atualiza um usuário -> PATCH ou PUT
+//show -> mostra um usuário -> GET
+//update -> atualiza um usuário -> PATCH ou PUT
 */
