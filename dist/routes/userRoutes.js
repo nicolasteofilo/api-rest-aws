ef52a5fcb7181fd931f92c5901ef81b286b018a7
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-08-20 15:23:56 -0300

    Add files

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -6,8 +6,8 @@ var _loginRequired = require('../middlewares/loginRequired'); var _loginRequired
 const router = new (0, _express.Router)();
 
 // Não deveria existir
-// router.get('/', userController.index); // Lista usuários
-// router.get('/:id', userController.show); // Lista usuário
+ router.get('/', _UserController2.default.index); // Lista usuários
+ router.get('/:id', _UserController2.default.show); // Lista usuário
 
 router.post('/', _UserController2.default.store);
 router.put('/', _loginRequired2.default, _UserController2.default.update);
